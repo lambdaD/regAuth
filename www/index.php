94374a8aa06e1de923b79d3253b500bd8914be50
johnBebra <94629740+johnBebra@users.noreply.github.com>
2023-07-21 23:55:23 +0500

    Форма регистрации

--- www/index.php
+++ www/index.php
@@ -6,14 +6,14 @@
   <title>Авторизация и регистрация</title>
 </head>
 <body>
-  <form action="">
+  <form action="" method="post">
     <label for="">Логин</label>
     <input type="text" placeholder="Введите свой логин">
     <label for="">Пароль</label>
     <input type="password" placeholder="Введите свой пароль">
     <button>Войти</button>
     <p>
-      <span>У вас нет аккаунта? <a href="#">Зарегистрируйтесь.</a></span>
+      <span>У вас нет аккаунта? <a href="reg.php">Зарегистрируйтесь.</a></span>
     </p>
   </form>
   
---
+++ www/reg.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="assets/css/main.css">
+  <title>Авторизация и регистрация</title>
+</head>
+<body>
+  <form action="" method="post">
+    <label for="">Фамилия</label>
+    <input type="text" placeholder="Введите свою фамилию">
+    <label for="">Имя</label>
+    <input type="text" placeholder="Введите своё имя">
+    <label for="">Логин</label>
+    <input type="text" placeholder="Введите свой логин">
+    <label for="">Email</label>
+    <input type="email" placeholder="Введите свой Email">
+    <label for="">Пароль</label>
+    <input type="password" placeholder="Введите свой пароль">
+    <label for="">Повторите пароль</label>
+    <input type="password" placeholder="Повторите пароль">
+    <label for="">Изображение профиля</label>
+    <input type="file">
+    <button>Зарегистрироваться</button>
+    <p>
+      <span>Уже есть аккаунт? <a href="index.php">Войдите.</a></span>
+    </p>
+  </form>
+  
+</body>
+</html>
