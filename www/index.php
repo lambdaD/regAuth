cd0d3bc38454974fe8a776de2f14ee70aa222b6d
johnBebra <94629740+johnBebra@users.noreply.github.com>
2023-07-21 22:01:34 +0500

    First Commit

---
+++ www/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="assets/css/main.css">
+  <title>Авторизация и регистрация</title>
+</head>
+<body>
+  <form action="">
+    <label for="">Логин</label>
+    <input type="text" placeholder="Введите свой логин">
+    <label for="">Пароль</label>
+    <input type="password" placeholder="Введите свой пароль">
+    <button>Войти</button>
+    <p>
+      <span>У вас нет аккаунта? <a href="#">Зарегистрируйтесь.</a></span>
+    </p>
+  </form>
+  
+</body>
+</html>
