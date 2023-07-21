69eb60c9542d207bad8cc0d5248a1d750376f03a
johnBebra <94629740+johnBebra@users.noreply.github.com>
2023-07-22 02:45:36 +0500

    upload files (without utf8)

--- www/vendor/signup.php
+++ www/vendor/signup.php
@@ -9,11 +9,19 @@
   $confirmPassword = $_POST['confirmPassword'];
 
   if ($password === $confirmPassword){
-
+    $path = 'uploads/'.time().$_FILES['avatar']['name'];
+    
+    if (!move_uploaded_file($_FILES['avatar']['tmp_name'], '../'.$path)) {
+      $_SESSION[message] = 'Ошибка при загрузке изображения';
+      header('Location: ../reg.php');
+    }
+    
   }
   else{
     $_SESSION[message] = 'Пароли не совпадают';
     header('Location: ../reg.php');
   }
+
+  
 
 ?>
