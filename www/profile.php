575941acc4999f0b652d1b19e79dbf1d41356e01
lambdaD <joyitdust@gmail.com>
2023-08-07 00:00:03 +0500

    посты работают

--- www/profile.php
+++ www/profile.php
@@ -21,6 +21,30 @@ if (!$_SESSION[user]) {
   <button type="submit">Создать запись</button>
   <a href="vendor/logout.php" class="logout">Выход</a>
   </form>
-  
+  <div class="posts">
+    <h3>Записи пользователя:</h3>
+
+    <?php
+    require_once('vendor/connect.php');
+    $user_id = $_SESSION["user"]["id"];
+    
+    $post = mysqli_query($connect, "SELECT * FROM `posts` WHERE `user_id` = '$user_id' ORDER BY `created_at` DESC");
+    while($fetchPost = mysqli_fetch_assoc($post)){
+      
+      echo "<p>" . $fetchPost['content'] . "</p>";
+      echo "<p>Дата создания: " . $fetchPost['created_at'] . "</p>";
+   }
+    // $stmt = $pdo->prepare($sql);
+    // $stmt->execute([$user_id]);
+    // $posts = $stmt->fetchAll();
+
+    // // Выводим все записи пользователя
+    // foreach ($posts as $post) {
+    //   echo "<p>" . $post["content"] . "</p>";
+    //   echo "<p>Дата создания: " . $post["created_at"] . "</p>";
+    // }
+    ?>
+
+  </div>
 </body>
 </html>
--- www/vendor/create_post.php
+++ www/vendor/create_post.php
@@ -5,6 +5,6 @@ require_once('connect.php');
 $content = $_POST['content'];
 $user_id = $_SESSION['user']['id'];
 mysqli_query($connect, "INSERT INTO `posts`(`user_id`, `content`, `created_at`) VALUES ('$user_id', '$content', NOW())");
-echo($content.'   '.$user_id );
-//header("Location: ../profile.php");
+// echo($content.'   '.$user_id );
+header("Location: ../profile.php");
 ?>
