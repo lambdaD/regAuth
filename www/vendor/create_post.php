044084245a2ca3ed4242098fed2c6ee09ff5a644
lambdaD <joyitdust@gmail.com>
2023-08-12 05:33:51 +0500

    м

--- www/vendor/create_post.php
+++ www/vendor/create_post.php
@@ -4,7 +4,10 @@ require_once('connect.php');
 
 $content = $_POST['content'];
 $user_id = $_SESSION['user']['id'];
-mysqli_query($connect, "INSERT INTO `posts`(`user_id`, `content`, `created_at`) VALUES ('$user_id', '$content', NOW())");
-// echo($content.'   '.$user_id );
-header("Location: ../profile.php");
+if ($content != '') {
+  mysqli_query($connect, "INSERT INTO `posts`(`user_id`, `content`, `created_at`) VALUES ('$user_id', '$content', NOW())");
+  // echo($content.'   '.$user_id );
+  header("Location: ../profile.php");
+}
+  header("Location: ../profile.php");
 ?>
